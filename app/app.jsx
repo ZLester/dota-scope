19b9e3b894b3f60fdebabbc5e75a6925f5864a01
Zachary Lester <zachary.greg.lester@gmail.com>
2015-11-10 17:37:00 -0800

    Add comments to App component

--- app/app.jsx
+++ app/app.jsx
@@ -8,7 +8,9 @@ var HeroSelector = require('./heroselector.jsx');
 var App = React.createClass({
   getInitialState () {
     return ({
+      // Initial/Default State for Hero MouseOver Message
       heroMouseOver: 'Select a Hero',
+      // Initial/Default State for Selected Hero Portraits
       selectorStates: {
         slotZero: '',
         slotOne: '',
@@ -19,13 +21,19 @@ var App = React.createClass({
     })
   },
   handleHeroClick (hero) {
+    // Check if hero is already in one of the slots
     for (var slotI in this.state.selectorStates) {
+      // If so, return to avoid duplicates
       if (this.state.selectorStates[slotI] === hero) {
         return;
       }
     }
+    // If not, iterate over slots again
     for (var slotJ in this.state.selectorStates) {
+      // Add hero to first empty slot encountered
       if (this.state.selectorStates[slotJ] === '') {
+        // Generate new state object and set
+        // Mutates here, refactor later
         var newStates = this.state.selectorStates;
         newStates[slotJ] = hero;
         this.setState({selectorStates: newStates});
@@ -33,17 +41,21 @@ var App = React.createClass({
       }
     }
   },
+  // MouseEnter Listener for HeroGrid 
   handleHeroMouseEnter (hero) {
     this.setState({heroMouseOver: hero})
   },
+  // MouseLeave Listener for HeroGrid 
   handleHeroMouseLeave () {
     this.setState({heroMouseOver: 'Select a Hero'})
   },
+  // Click Listener for Selected Hero Portrait
   handleSlotClick (slot) {
     var newStates = this.state.selectorStates;
     newStates[slot] = '';
     this.setState({selectorStates: newStates});
   },
+  // Click Listener for Clear Button
   handleClearClick () {
     var newStates = {
       slotZero: '',
