5b1254201c498c1421ee767687eca62aef5556ef
Zachary Lester <zachary.greg.lester@gmail.com>
2015-11-11 03:57:10 -0800

    Modify how background image is displayed, alter title casing

--- app/title.jsx
+++ app/title.jsx
@@ -3,7 +3,9 @@ var React = require('react');
 var Title = React.createClass({
   render () {
     return (
-      <div className="titleWrapper"><h1 className="title">Dota Scope</h1></div>
+      <div className="titleWrapper">
+        <h1 className="title">DOTA SCOPE</h1>
+      </div>
     )
   }
 });
