c71875ecf84aa69c9bc99d0ff76833500ddc488d
Zachary Lester <zachary.greg.lester@gmail.com>
2015-11-15 12:56:52 -0800

    Server code cleanup

--- server/index.js
+++ server/index.js
@@ -2,13 +2,13 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var app = express();
 var path = require('path');
+var port = process.env.PORT || 3000;
+
 var apiRouter = require('./router/apirouter.js');
-var port = process.env.PORT || 3000
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/api', apiRouter);
 app.use(express.static(path.join(__dirname, "../build")));
-console.log('Dota Scope Listening on port ' + port);
 
 app.listen(port);
